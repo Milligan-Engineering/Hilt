aaedf4214e36bbde6f945d52046d8138e821bb54
Brandon Kelly <blkelly@my.milligan.edu>
2020-01-27 10:08:49 -0500

    Added input area for File Directory Location

--- Hilt/Source.cpp
+++ Hilt/Source.cpp
@@ -3,5 +3,7 @@ using namespace std;
 int main()
 {
 	//Declaration of Variables
-	char fileDirectory;
+	char fileDirectory,assignmentName,className;
+	cout << "What File Directory do you want to edit?\n";
+	cin >> fileDirectory;
 };
