d82ff4e8038369a114896b767907ca99ffccedf5
Brandon Kelly <blkelly@my.milligan.edu>
2020-01-27 21:01:57 -0500

    Added section for user input and basic conditional programming.

--- Hilt/Source.cpp
+++ Hilt/Source.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 using namespace std;
+//Declaration of Variables
+string fileDirectory, assignmentName, className;
+int i = 0;//Counter Variable
+//Create Functions for storing of variables
+string directoryEntry(int x)
+{
+	if (x == 0)
+	{
+		cout << "What File Directory do you want to edit?\n";
+		cin >> fileDirectory;
+		i++;
+		//return i;
+		return fileDirectory;
+	}
+	else
+	{
+		cout << "Please enter correct File Directory.\n";
+		cin >> fileDirectory;
+		return fileDirectory;
+	}
+	//return i
+};
 int main()
 {
-	//Declaration of Variables
-	char fileDirectory,assignmentName,className;
-	cout << "What File Directory do you want to edit?\n";
-	cin >> fileDirectory;
+	string userAnswer;
+	//Store User input in variables
+	directoryEntry(i);
+	cout << "You entered " << fileDirectory << ".  Enter Yes if this is the correct file directory?\n";
+	cin >> userAnswer;
+	if (userAnswer != "Yes") {
+		directoryEntry(i);
+		cout << "Thank You\n";
+	}
+	else
+	{
+		cout << "Thank You";
+	}
+	//Create
 };
