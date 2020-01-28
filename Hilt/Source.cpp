ca886b50972b5f3d7b111caf2ceb28901edd47e8
Brandon Kelly <blkelly@my.milligan.edu>
2020-01-28 12:28:33 -0500

    Added input functions

--- Hilt/Source.cpp
+++ Hilt/Source.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 //Declaration of Variables
 string fileDirectory, assignmentName, className;
-int i = 0;//Counter Variable
+int i = 0, j=0, k=0;//Counter Variable
 //Create Functions for storing of variables
 string directoryEntry(int x)
 {
@@ -22,20 +22,54 @@ string directoryEntry(int x)
 	}
 	//return i
 };
+string classEntry(int x)
+{
+	if (x == 0)
+	{
+		cout << "What is the class name?\n";
+		cin >> className;
+		j++;
+		return className;
+	}
+	else
+	{
+		cout << "Please enter correct name\n";
+		cin >> className;
+		return className;
+	}
+};
+string assignEntry(int x)
+{
+	if (x == 0)
+	{
+		cout << "What is the assignment name?\n";
+		cin >> assignmentName;
+		k++;
+		return assignmentName;
+	}
+	else
+	{
+		cout << "Please enter correct name\n";
+		cin >> assignmentName;
+		return assignmentName;
+	}
+};
 int main()
 {
-	string userAnswer;
+	string userAnswerClass,userAnswerAssignment,userAnswerDir;
 	//Store User input in variables
+	classEntry(j);
+	assignEntry(k);
 	directoryEntry(i);
 	cout << "You entered " << fileDirectory << ".  Enter Yes if this is the correct file directory?\n";
-	cin >> userAnswer;
-	if (userAnswer != "Yes") {
+	cin >> userAnswerDir;
+	if (userAnswerDir != "Yes") {
 		directoryEntry(i);
 		cout << "Thank You\n";
 	}
 	else
 	{
-		cout << "Thank You";
-	}
-	//Create
+		cout << "Thank You\n";
+	};
+	cout << "You entered:\n" << "Class Name: " << className << endl << "Assignment Name: " << assignmentName << endl << "File Location: " << fileDirectory << endl;
 };
