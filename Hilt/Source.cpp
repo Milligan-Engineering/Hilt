53e81be66a05fbf5a38825bd57064bdf73e4a1cd
Brandon Kelly <blkelly@my.milligan.edu>
2020-01-29 23:27:11 -0500

    Added areas for user input and a simple calculation

--- Hilt/Source.cpp
+++ Hilt/Source.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 using namespace std;
+//Declaration of Variables
+string className, assignmentName, fileDirectory;
+int numberStudents, numberAssignments, totalAssignments;
 int main()
 {
-	//Declaration of Variables
-	char fileDirectory;
-	//test
+	cout << "Welcome to H.I.L.T!\n To start we are going to have you input a few basic pieces of information:\n";
+	/*This for the calculation part of the assignment.  
+	Likely will not last longterm or will be moved to more approriate part of the program. 
+	This feature could be reworked for checking to make sure that complete set of assignments have been turned-in/scanned at the end of the year,
+	but is unnessacary to the current proposed functionality of the program.  Any thoughts?*/
+	cout << "How many students are in this class?\n";
+	cin >> numberStudents;
+	cout << "How many assignments per student?\n";
+	cin >> numberAssignments;
+	totalAssignments = numberAssignments * numberStudents;
+	cout << "There should be a total of " << totalAssignments << " files this semester for this class.\n";
+	/*This is the section for the user to input the basic information for the labeler.  Text entry will likely not be the ideal input system, 
+	especially for file directory and class names, but should be relatively simple to get working or replace*/
+	cout << "What directory do you want to rename files in?\n";
+	cin >> fileDirectory;
+	cout << "What is the name of the class (using dashes or underscores for spaces)?\n";
+	cin >> className;
+	cout << "What assignment is this(using dashes or underscores for spaces)?\n";
+	cin >> assignmentName;
+	cout << "Thank you!\n All of your files will have this format:\n" << fileDirectory << "\\" << className << "-" << assignmentName << "-firstName-lastName.fileExtension\n";
+	return 0;
 };
