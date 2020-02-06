00917df9713a1e4fe56e11a6ee112f6b22eb91af
Brandon Kelly <blkelly@my.milligan.edu>
2020-02-05 22:05:06 -0500

    Added quite a few things: array for storing student names while loops for both error checking and data entry and some if statements.

--- Hilt/Hilt.cpp
+++ Hilt/Hilt.cpp
@@ -6,6 +6,9 @@ using namespace std;
 string className, assignmentName, fileDirectory;
 int numberStudents, numberAssignments, totalAssignments;
 char userInput;
+int const MAX_STUDENTS = 6;
+//Declaration of array
+string studentName[6];//Six is a placeholder value as it is the number of students EENG 221
 int main()
 {
 	//Greeting Message.  Will likely be expanded to add some basic rules/instructions
@@ -14,7 +17,7 @@ int main()
 	Likely will not last longterm or will be moved to more approriate part of the program. 
 	This feature could be reworked for checking to make sure that complete set of assignments have been turned-in/scanned at the end of the year,
 	but is unnessacary to the current proposed functionality of the program.  Any thoughts?*/
-	cout << "Would you like to get an estimate for the amount of files you will have at the end of the semester?\n Type y for yes or anything else for no\n";
+	cout << "Would you like to get an estimate for the amount of files you will have at the end of the semester?\n Type y for yes or n for no\n";
 	cin >> userInput;
 	if (userInput == 'y')
 	{
@@ -25,7 +28,22 @@ int main()
 		totalAssignments = numberAssignments * numberStudents;
 		cout << "There should be a total of " << totalAssignments << " files this semester for this class.\n";
 	}
-
+	//While loop to populate array with student names
+	int j = 0;
+	while (j < MAX_STUDENTS)
+	{
+		if (j == 0)
+		{
+			cout << "What is the first student's name?\n";
+			cin >> studentName[j];
+		}
+		else
+		{
+			cout << "What is the next students name?\n";
+			cin >> studentName[j];
+		}
+		j++;
+	}
 	/*This is the section for the user to input the basic information for the labeler.  Text entry will likely not be the ideal input system, 
 	especially for file directory and class names, but should be relatively simple to get working and/or replace*/
 	//User inputs.  File directory will possibly be reworked into original file directory and the option to move the files while renaming them
@@ -42,5 +60,13 @@ int main()
 	cin >> assignmentName;
 	//Final output.
 	cout << "Thank you!\n All of the selected files will have this format:\n" << fileDirectory << "\\" << className << "-" << assignmentName << "-firstName-lastName.fileExtension\n";
+	//I am just outputting to the console for now, but this will be changed to rename the files
+	int i = 0;
+	cout << "Here are the file names:\n";
+	while (i < 6)
+	{
+		cout << fileDirectory << "\\" << className << "-" << assignmentName << "-" << studentName[i] << ".fileExtension\n";
+		i++;
+	}
 	return 0;
 };
