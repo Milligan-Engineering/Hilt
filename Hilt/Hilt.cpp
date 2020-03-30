a279312f2146ea4e88d5f5715a1526fee949745f
Brandon Kelly <blkelly@my.milligan.edu>
2020-03-30 09:00:19 -0400

    Fixed the input checker functions

--- Hilt/Hilt.cpp
+++ Hilt/Hilt.cpp
@@ -7,7 +7,7 @@
 //Last Changed: 3/25/2020
 #include <iostream>
 #include<fstream>
-//#include "C:\Users\weasa\source\repos\fast-cpp-csv-parser\csv.h"
+#include<Windows.h>
 using namespace std;
 //Declaration of Variables
 string /*className,*/ assignmentName, fileDirectory, termName, classFileName;
@@ -27,41 +27,55 @@ string classTwoFiles[MAX_STUDENTS] = { "astonishing", "astounding", "blindsiding
 //Declartion of functions
 void studentNameInput();//Allows for the user to input student names.  
 string validator(string input, string desiredInput);
+//Precondition: String you want to check and desired string/characters to compare against
+//Postcondition: The function compares the two and returns the user input when they are the same as a string
+string inputChecker(string input);
+int inputChecker(int input);
+double inputChecker(double input);
+bool inputChecker(bool input);
+char inputChecker(char input);
 //Preconditon: The input of you want the user to enter
 //Postconditon: A validated input from the user will be stored in the input entered
-void inputChecker(string input);
-void inputChecker(int input);
-void inputChecker(double input);
-void inputChecker(bool input);
-void inputChecker(char input);
-//Precondition: String you want to check and desired string/characters to compare against
-//Post-Condition: The function compares the two and returns the user input when they are the same as a string
+void colorChanger(int colorNumber);
+//Precondition: Place the number of the color you want the following text to be.
+//Postconditon:The output color is changed
+
 string confirmer(string nameOfInput,string userInput);
 //Precondition: What input you need to the user to confirm and what it is called in the program, where the userInput is a string and the name of the variable with type string
 //Post-Condition: Returns the confirmed user input as a string.
 int main()
 {
 	//Greeting Message.  Will likely be expanded to add some basic rules/instructions
-	cout << "Welcome to H.I.L.T!\n To start we are going to have you input a few basic pieces of information:\n";
+	colorChanger(11);
+	cout << "Welcome to H.I.L.T!\nTo start we are going to have you input a few basic pieces of information:\n";
 	/*This for the calculation part of the assignment.  
 	Likely will not last longterm or will be moved to more approriate part of the program. 
 	This feature could be reworked for checking to make sure that complete set of assignments have been turned-in/scanned at the end of the year,
 	but is unnessacary to the current proposed functionality of the program.  Any thoughts?*/
+	colorChanger(15);
 	cout << "What term are we in?\n";
+	colorChanger(10);
 	//cin >> termName;//Will hopefuully be embedded into class list files.
-	inputChecker(termName);		
+	termName = inputChecker(termName);		
+	colorChanger(15);
 	cout << "How many students are in this class?\n";
 	//cin >> numberStudents;
-	inputChecker(numberStudents);
+	colorChanger(10);
+	numberStudents = inputChecker(numberStudents);
+	colorChanger(15);
 	cout << "Would you like to get an estimate for the amount of files you will have at the end of the semester?\n Type y for yes or any other letter for no\n";
 	//cin >> userInputCalc;
-	inputChecker(userInputCalc);
+	colorChanger(10);
+	userInputCalc = inputChecker(userInputCalc);
 	userInputCalc = tolower(userInputCalc);
 	if (userInputCalc == 'y')
 	{
+		colorChanger(15);
 		cout << "How many assignments per student?\n";
-		cin >> numberAssignments;
+		colorChanger(10);
+		numberAssignments = inputChecker(numberAssignments);
 		totalAssignments = numberAssignments * numberStudents;
+		colorChanger(15);
 		cout << "There should be a total of " << totalAssignments << " files this semester for this class.\n";
 	}
 	//While loop to populate array with student names
@@ -86,14 +100,17 @@ int main()
 	do {
 		if (l > 0)
 		{
+			colorChanger(15);
 			"Sorry that is an invalid input, please try again.\n";
 			cin.clear();
 			cin.ignore();
 		}
+		colorChanger(15);
 		cout << "What directory do you want to rename files in?\n";
 		cout << "1:USB Drive\n";
 		cout << "2:Documents\n";
 		cout << "3:Custom Location\n";
+		colorChanger(10);
 		cin >> fileLocation;
 		l++;
 	} while (cin.fail());
@@ -101,23 +118,29 @@ int main()
 	{
 		case 1:
 		{
+			colorChanger(15);
 			fileDirectory = "F:\\scannedFiles";
 			break;
 		}
 		case 2:
 		{
+			colorChanger(15);
 			fileDirectory = "C:\\Documents";
 			break;
 		}
 		case 3:
+			colorChanger(15);
 			cout << "What file directory do you want to read and write from?\n";
+			colorChanger(10);
 			cin >> fileDirectory;
 			validator(fileDirectory, "test");
 	}
 	//Below are loops for the user to confirm the name of the class and the assignment
 	//className = confirmer("class", className);
+	colorChanger(15);
 	cout << "Class Name please? Use return to stop input\n";
 	cout << "Class name:";
+	colorChanger(10);
 	cin.ignore();
 	cin.getline(className,100);
 	classFileName =  "test.csv";
@@ -137,13 +160,17 @@ int main()
 		count++;
 	}
 	classFile.close();
+	colorChanger(15);
 	cout << "Is this Class 1 or Class 2? (Please enter the number)\n";
 	//cin >> classIndication;
-	inputChecker(classIndication);
+	colorChanger(10);
+	classIndication = inputChecker(classIndication);
 	assignmentName = confirmer("assignment", assignmentName);
 	//Final output.
+	colorChanger(15);
 	cout << "Thank you!\n All of the selected files will have this format:\n" << fileDirectory << "\\" << className << "-" << assignmentName << "-firstName-lastName.fileExtension\n";
 	//I am just outputting to the console for now, but this will be changed to rename the files
+	colorChanger(15);
 	cout << "Here are the file names:\n";
 	ofstream newFileName;
 	newFileName.open("fileNames.txt");
@@ -188,12 +215,16 @@ void studentNameInput() //Allows for student name inputs to be implemented outsi
 	{
 		if (j == 0)
 		{
+			colorChanger(15);
 			cout << "What is the first student's name?\n";
+			colorChanger(10);
 			cin >> studentName[j];
 		}
 		else
 		{
+			colorChanger(15);
 			cout << "What is the next students name?\n";
+			colorChanger(10);
 			cin >> studentName[j];
 		}
 
@@ -213,9 +244,15 @@ string confirmer(string nameOfInput, string userInput)
 	char userConfirmation;
 	do
 	{
+		colorChanger(15);
 		cout << "Please enter your desired " << nameOfInput << " name (using dashes or underscores for spaces.)\n";
+		colorChanger(10);
 		cin >> userInput;
-		cout << "Is this the correct " << nameOfInput<<" name?\n" << userInput << " \n";
+		colorChanger(15);
+		cout << "Is this the correct " << nameOfInput << " name?\n";
+		colorChanger(10); 
+		cout << userInput << " \n";
+		colorChanger(10);
 		cin >> userConfirmation;
 		userConfirmation = tolower(userConfirmation);
 	} while (userConfirmation != 'y');
@@ -229,6 +266,7 @@ void csvParser(string fileName)
 	dataFile.get(currentChar);
 	if (dataFile.fail())
 	{
+		colorChanger(12);
 		cout << "\nOpening class data file failed\n";
 		exit(1);
 	}
@@ -236,6 +274,7 @@ void csvParser(string fileName)
 	nameList.open("names.txt");
 	if (nameList.fail())
 	{
+		colorChanger(12);
 		cout << "\nCreating name file failed\n";
 		exit(1);
 	}
@@ -244,73 +283,102 @@ void csvParser(string fileName)
 
 	}
 }
-void inputChecker(string input)
+string inputChecker(string input)
 {
 	cin >> input;
-	cout << cin.fail()<<endl;
+	colorChanger(14);
+	cout << "\n Status of input(0 means success and 1 means improper input): "<< cin.fail() << endl;
 	while (cin.fail())
 	{
+		colorChanger(10);
 		cin >> input;
 		//cout << cin.fail() << endl;
 		cin.clear();
 		cin.ignore();
+		colorChanger(12);
 		cout << "Sorry but that input is not valid, please use the requested input type\n";
+		colorChanger(10);
 		cin >> input;
 	}
+	return input;
 }
-void inputChecker(int input)
+int inputChecker(int input)
 {
 	cin >> input;
-	cout << cin.fail()<<endl;
-	while (cin.fail())
+	colorChanger(14);
+	cout << "\n Status of input(0 means success and 1 means improper input): " << cin.fail() << endl;	while (cin.fail())
 	{
+		colorChanger(10);
 		cin >> input;
 		//cout << cin.fail() << endl;
 		cin.clear();
 		cin.ignore();
+		colorChanger(12);
 		cout << "Sorry but that input is not valid, please use the requested input type\n";
+		colorChanger(10);
 		cin >> input;
 	}
+	return input;
 }
-void inputChecker(double input)
+
+double inputChecker(double input)
 {
 	cin >> input;
-	cout << cin.fail()<<endl;
+	cout << "\n Status of input(0 means success and 1 means improper input): " << cin.fail() << endl;
 	while (cin.fail())
 	{
+		colorChanger(10);
 		cin >> input;
 		//cout << cin.fail() << endl;
 		cin.clear();
 		cin.ignore();
+		colorChanger(12);
 		cout << "Sorry but that input is not valid, please use the requested input type\n";
+		colorChanger(10);
 		cin >> input;
 	}
+	return input;
 }
-void inputChecker(bool input)
+
+bool inputChecker(bool input)
 {
 	cin >> input;
-	cout << cin.fail()<<endl;
+	colorChanger(14);
+	cout << "\n Status of input(0 means success and 1 means improper input): " << cin.fail() << endl;
 	while (cin.fail())
 	{
+		colorChanger(10);
 		cin >> input;
 		//cout << cin.fail() << endl;
 		cin.clear();
 		cin.ignore();
+		colorChanger(12);
 		cout << "Sorry but that input is not valid, please use the requested input type\n";
+		colorChanger(10);
 		cin >> input;
 	}
+	return input;
 }
-void inputChecker(char input)
+char inputChecker(char input)
 {
 	cin >> input;
-	cout << cin.fail()<<endl;
+	colorChanger(14);
+	cout << "\n Status of input(0 means success and 1 means improper input): " << cin.fail() << endl;
 	while (cin.fail())
 	{
+		colorChanger(10);
 		cin >> input;
 		//cout << cin.fail() << endl;
 		cin.clear();
 		cin.ignore();
+		colorChanger(12);
 		cout << "Sorry but that input is not valid, please use the requested input type\n";
+		colorChanger(10);
 		cin >> input;
 	}
+	return input;
+}
+void colorChanger(int colorNumber)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorNumber);
 }
